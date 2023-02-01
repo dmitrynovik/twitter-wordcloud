d3360f3116677fddf83ba55bfcd67a49f38ab0b9
dmitrtynovik <cryinstone@gmail.com>
2023-02-01 14:22:20 +1100

    feat: adding stream queue

--- modelviewcontroller/src/main/java/jp/vmware/tanzu/twitterwordcloud/modelviewcontroller/configuration/MvcMQConfiguration.java
+++ modelviewcontroller/src/main/java/jp/vmware/tanzu/twitterwordcloud/modelviewcontroller/configuration/MvcMQConfiguration.java
@@ -1,5 +1,7 @@
 package jp.vmware.tanzu.twitterwordcloud.modelviewcontroller.configuration;
 
+import java.util.Collections;
+
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.FanoutExchange;
@@ -14,8 +16,8 @@ import org.springframework.context.annotation.Configuration;
 public class MvcMQConfiguration {
 
 	public static final String EXCHANGE_NAME = "tweet-fanout";
-
 	public static final String QUEUE_NAME = "tweet-handler";
+	public static final String STREAM_NAME = "tweet-handler-stream";
 
 	@Value("notification-${random.uuid}")
 	public String notificationQueue;
@@ -40,6 +42,16 @@ public class MvcMQConfiguration {
 	}
 
 	@Bean
+	public Queue streamQueue() {
+		return new Queue(STREAM_NAME, 
+			true, 
+			false, 
+			false, 
+			Collections.singletonMap("x-queue-type", "stream")
+		);
+	}
+
+	@Bean
 	Binding tweetQueueBinding(Queue tweetQueue, FanoutExchange mvcExchange) {
 		return BindingBuilder.bind(tweetQueue).to(mvcExchange);
 	}
@@ -47,6 +59,11 @@ public class MvcMQConfiguration {
 	@Bean
 	Binding notificationQueueBinding(Queue notificationQueue, FanoutExchange exchange) {
 		return BindingBuilder.bind(notificationQueue).to(exchange);
+	}
+
+	@Bean
+	Binding streamQueueBinding(Queue streamQueue, FanoutExchange mvcExchange) {
+		return BindingBuilder.bind(streamQueue).to(mvcExchange);
 	}
 
 }
