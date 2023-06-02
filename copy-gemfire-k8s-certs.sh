9f7e745fa014515b810db9e2507e989a7d03d40a
dmitrynovik <cryinstone@gmail.com>
2023-06-01 19:25:46 -0700

    feat: parametrizing copy K8s GF certs

--- copy-gemfire-k8s-certs.sh
+++ copy-gemfire-k8s-certs.sh
@@ -1,3 +1,6 @@
-kubectl get secret -n tanzu-gemfire gemfire-cluster-cert -o=jsonpath='{.data.password}' | base64 --decode > ./gemfire-certs/password
-kubectl get secret -n tanzu-gemfire gemfire-cluster-cert -o=jsonpath='{.data.keystore\.p12}' | base64 --decode > ./gemfire-certs/keystore.p12
-kubectl get secret -n tanzu-gemfire gemfire-cluster-cert -o=jsonpath='{.data.truststore\.p12}' | base64 --decode > ./gemfire-certs/truststore.p12
+NAMESPACE="tanzu-gemfire"
+TARGET_FOLDER="./gemfire-certs"
+
+kubectl get secret -n $NAMESPACE gemfire-cluster-cert -o=jsonpath='{.data.password}' | base64 --decode > "$TARGET_FOLDER/password"
+kubectl get secret -n $NAMESPACE gemfire-cluster-cert -o=jsonpath='{.data.keystore\.p12}' | base64 --decode > "$TARGET_FOLDER/keystore.p12"
+kubectl get secret -n $NAMESPACE gemfire-cluster-cert -o=jsonpath='{.data.truststore\.p12}' | base64 --decode > "$TARGET_FOLDER/truststore.p12"
